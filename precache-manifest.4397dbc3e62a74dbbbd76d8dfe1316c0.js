cd9d7ae6fbea4930f5bba775313e8bf2de8fa18d
Tyler Fredrickson <tylerdavidfred@gmail.com>
2020-01-02 05:30:31 -0600

    Updates

---
+++ precache-manifest.4397dbc3e62a74dbbbd76d8dfe1316c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df08f85fbb0a3c2618ce63a5e3fc8f87",
+    "url": "/facerecognition/index.html"
+  },
+  {
+    "revision": "d10b3125f4ad389615da",
+    "url": "/facerecognition/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d5677c4dbdea4a41a2f4",
+    "url": "/facerecognition/static/js/2.d5a020f5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/facerecognition/static/js/2.d5a020f5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d10b3125f4ad389615da",
+    "url": "/facerecognition/static/js/main.1396b9e2.chunk.js"
+  },
+  {
+    "revision": "726491fba0cd6cd902b7",
+    "url": "/facerecognition/static/js/runtime-main.8c449b4c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/facerecognition/static/media/logo.5d5d9eef.svg"
+  }
+]);
